3d0205f6ecd98233b4dba324e5881bd0f34d1fa6
Mustafa Raja <95049133+Mustafa-raja@users.noreply.github.com>
2022-08-18 03:55:37 +0500

    Delete Portfolio directory

--- Portfolio/src/main/java/com/example/demo/CredsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CredsRepo extends JpaRepository<creds, String> {
-}
